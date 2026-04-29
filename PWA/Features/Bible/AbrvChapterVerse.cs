a6665320bfb7da50ec63f7d86bf95e057200219d
John Marsing <JohnMarsing@outlook.com>
2026-04-28 18:26:02 -0700

    add basic vertical scroll spy component, fix bible route

--- PWA/Features/Bible/AbrvChapterVerse.cs
+++ PWA/Features/Bible/AbrvChapterVerse.cs
@@ -1,10 +1,13 @@
 ﻿using HomeHelper = PWA.Features.Bible.Helper;
+using PWA.Enums;
+
 namespace PWA.Features.Bible;
 
 public record AbrvChapterVerse(string Abrv, int Chapter, int Verse, bool VerseIsNotDefault, int ScriptureId, int LastVerse)
 {
 	public static AbrvChapterVerse Default => new(
 		RCL.Enums.BibleBook.Genesis.Name, 1, 1, false, 1, HomeHelper.LastVerseCount(RCL.Enums.BibleBook.Genesis, 1));
-	//                                                                    
+	                                                                
+	public string NavigateToUrl() => $"{Nav.Bible.Index}/{Abrv}/{Chapter}/{Verse}/{ScriptureId}"; 
 }
 
