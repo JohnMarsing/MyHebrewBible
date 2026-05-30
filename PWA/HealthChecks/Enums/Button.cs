6c0c59c9c247e19a495dc5d9795a3fa5a79d5b96
John Marsing <JohnMarsing@outlook.com>
2026-05-29 18:28:10 -0700

    Fixed Filtered Strong's hyper link if IsXs

--- PWA/HealthChecks/Enums/Button.cs
+++ PWA/HealthChecks/Enums/Button.cs
@@ -8,7 +8,6 @@ public enum GroupType
 	Sentry = 2
 }
 
-
 public abstract class Button : SmartEnum<Button>
 {
 	#region Id's
@@ -19,7 +18,7 @@ public abstract class Button : SmartEnum<Button>
 
 		internal const int Throw = 3;
 		internal const int ThrowAndCatch = 4;
-
+		internal const int TestSqlite = 5;
 	}
 	#endregion
 
@@ -27,8 +26,8 @@ public abstract class Button : SmartEnum<Button>
 	public static readonly Button TableRowCntTable = new TableRowCntTableSE();
 	public static readonly Button Gen01 = new Gen01SE();
 	public static readonly Button Throw = new ThrowSE();
-	public static readonly Button ThrowAndCatch = new ThrowAndCatchSE	();
-
+	public static readonly Button ThrowAndCatch = new ThrowAndCatchSE();
+	public static readonly Button TestSqlite = new TestSqliteSE();
 	#endregion
 
 	private Button(string name, int value) : base(name, value)
@@ -39,14 +38,13 @@ public abstract class Button : SmartEnum<Button>
 	public abstract GroupType GroupTypeEnum { get; }
 	public abstract string Title { get; }
 	public abstract string Icon { get; }
-
 	#endregion
 
 	private sealed class TableRowCntTableSE : Button
 	{
 		public TableRowCntTableSE() : base(nameof(TableRowCntTable), Id.TableRowCntTable) { }
 		public override GroupType GroupTypeEnum => GroupType.Database;
-		public override string Title => "Table Row Count"; 
+		public override string Title => "Table Row Count";
 		public override string Icon => "fa-solid fa-table";
 	}
 
@@ -54,24 +52,31 @@ public abstract class Button : SmartEnum<Button>
 	{
 		public Gen01SE() : base(nameof(Gen01), Id.Gen01) { }
 		public override GroupType GroupTypeEnum => GroupType.Database;
-		public override string Title => "Book & Chapter Gen-01"; // 
-		public override string Icon => "fa-solid fa-minus"; 
+		public override string Title => "Book & Chapter Gen-01";
+		public override string Icon => "fa-solid fa-minus";
 	}
 
 	private sealed class ThrowSE : Button
 	{
 		public ThrowSE() : base(nameof(Throw), Id.Throw) { }
 		public override GroupType GroupTypeEnum => GroupType.Sentry;
-		public override string Title => "Throw (no catch)"; // 
+		public override string Title => "Throw (no catch)";
 		public override string Icon => "fa-solid fa-bomb";
 	}
 
-
 	private sealed class ThrowAndCatchSE : Button
-	{
+	{	
 		public ThrowAndCatchSE() : base(nameof(ThrowAndCatch), Id.ThrowAndCatch) { }
 		public override GroupType GroupTypeEnum => GroupType.Sentry;
-		public override string Title => "Throw and Catch"; // 
+		public override string Title => "Throw and Catch";
 		public override string Icon => "fa-solid fa-arrow-rotate-left";
+	}
+
+	private sealed class TestSqliteSE : Button
+	{
+		public TestSqliteSE() : base(nameof(TestSqlite), Id.TestSqlite) { }
+		public override GroupType GroupTypeEnum => GroupType.Database;
+		public override string Title => "Test Sqlite";
+		public override string Icon => "fa-solid fa-database";
 	}
 }
