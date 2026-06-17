07d7cd0adebacb449a09a2dd4f166604e4970ebb
John Marsing <JohnMarsing@outlook.com>
2026-06-16 18:52:58 -0700

    deleted BibleXref folder and CopySqlLiteDB folder

--- CopySqliteDB/TableDefinitions/WordPartKjv.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [WordPartKjv] (
-  [ScriptureID] int NOT NULL
-, [WordCount] tinyint NOT NULL
-, [Strongs] int NULL
-, [Word] nvarchar(70) NULL COLLATE NOCASE
-, CONSTRAINT [sqlite_autoindex_WordPartKjv_1] PRIMARY KEY ([ScriptureID],[WordCount])
-);
